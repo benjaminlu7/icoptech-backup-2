18a614d3584c5a8c77a7b234d9868b27fbac4496
Benjamin Lu <77364151+benjaminlu7@users.noreply.github.com>
2021-06-15 14:52:11 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Initiator ( index.php )
+ *
+ * @package   Initiator
+ * @copyright Copyright (C) 2019-2021. Benjamin Lu
+ * @license   GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author    Benjamin Lu ( https://getbenonit.com )
+ */
+?>
+<?php get_header(); ?>
+	<section id="content" class="site-content">
+		<div id="layout" class="<?php echo esc_attr( get_theme_mod( 'global_layout', 'no-sidebar' ) ); ?>">
+			<main id="main" class="content-area">
+				<?php
+					if ( have_posts() ) :
+						while ( have_posts() ) : the_post();
+							Benlumia007\Backdrop\Template\get_template_part( 'content/content', get_post_format() );
+						endwhile;
+						the_posts_pagination();
+					else :
+							Benlumia007\Backdrop\Template\get_template_part( 'content/content', 'none' );
+					endif;
+				?>
+			</main>
+			<?php Benlumia007\Backdrop\View\display( 'sidebar', [ 'primary' ] ); ?>
+		</div>
+	</section>
+<?php get_footer(); ?>
